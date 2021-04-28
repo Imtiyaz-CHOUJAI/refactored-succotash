e2cf636ffb8c3ce0007149c9626a78e3f651c7b3
safaendama <83344587+safaendama@users.noreply.github.com>
2021-04-28 12:11:10 +0000

    add fucntion multiplication

--- index.php
+++ index.php
@@ -1,8 +1,17 @@
 <?php
-
+// sum
 function sum(int $var1, int $var2)
 {
     return $var1 + $var2;
 }
 
 echo sum(1, 3);
+// multiplication 
+<?php
+
+function mul(int $var1, int $var2)
+{
+    return $var1 * $var2;
+}
+
+echo mul(1, 3);
